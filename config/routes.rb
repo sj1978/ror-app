65db179956bff64559164bc19f6009f3799684d0
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-04-07 23:35:52 -0300

    final changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about'
   # Defines the root path route ("/")
-   root "home#index"
+   root "friends#index"
 end
